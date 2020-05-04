c11bb01fa4d56b7aaeb377e56f97ef4941257d04
SahanWickramage <sahanwickramage.16@cse.mrt.ac.lk>
2020-05-04 14:57:27 +0530

    added "clock_divider.vhdl" and "clock_divider_tb.vhld"

---
+++ HDL_lab1-2.srcs/sources_1/imports/HDL_lab1.srcs/sim_1/new/clock_divider_tb.vhd
@@ -0,0 +1,52 @@
+library IEEE;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
+
+entity clock_divider_tb is
+end;
+
+architecture bench of clock_divider_tb is
+
+  component clock_divider
+      port ( clk      : in std_logic;
+             reset    : in std_logic;
+             one_hz_enable: out std_logic);
+  end component;
+
+  signal clk: std_logic := '0';
+  signal reset: std_logic := '0';
+  signal one_hz_enable: std_logic;
+
+  constant clock_period: time    := 10 ns;
+  signal stop_the_clock: boolean := false;
+
+begin
+
+  uut: clock_divider port map ( clk           => clk,
+                                reset         => reset,
+                                one_hz_enable => one_hz_enable );
+
+  stimulus: process
+  begin
+  
+    -- Put initialisation code here
+    -- wait for 100 ns;
+    -- stop_the_clock <= false;
+    
+    wait for 500 ns;
+    -- Put test bench stimulus code here
+    
+    stop_the_clock <= true;
+    wait;
+  end process;
+
+  clocking: process
+  begin
+    while not stop_the_clock loop
+      clk <= '0', '1' after clock_period / 2;
+      wait for clock_period;
+    end loop;
+    wait;
+  end process;
+
+end;
---
+++ HDL_lab1-2.srcs/sources_1/imports/HDL_lab1.srcs/sources_1/new/clock_divider.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+  
+entity clock_divider is
+    port ( clk      : in std_logic;
+           reset    : in std_logic;
+           one_hz_enable: out std_logic);
+end clock_divider;
+  
+architecture Behavioral of clock_divider is
+    signal count: integer         := 0;
+    signal temp_value : std_logic := '0';
+  
+begin
+  
+process(clk, reset)
+
+begin
+
+    if(reset = '1') then
+        count <= 0;
+        temp_value <= '0';
+    elsif rising_edge ( clk ) then
+        count <= count + 1;
+        -- if (count = 50_000_000) then -- f --> f/100, 100MHz --> 1Hz
+        if (count = 5) then -- f --> f/10, 100MHz --> 10MHz
+            temp_value <= NOT temp_value;
+            count <= 0;
+        end if;
+    end if;
+    
+    one_hz_enable <= temp_value;
+    
+end process;
+  
+end Behavioral;
