c93d580f1553401e3a5815955d4839c7a311c4c3
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2020-05-09 12:04:45 +0530

    modified git ignore and walk_register

--- HDL_lab1-2.srcs/sim_1/new/walk_register_tb.vhd
+++ HDL_lab1-2.srcs/sim_1/new/walk_register_tb.vhd
@@ -8,21 +8,24 @@ end;
 architecture bench of walk_register_tb is
 
   component walk_register
-      Port ( WR_Sync : in STD_LOGIC;
+      Port ( clk : in std_logic;
+             WR_Sync : in STD_LOGIC;
              WR_Reset : in STD_LOGIC;
              WR : out STD_LOGIC);
   end component;
 
+  signal clk: std_logic;
   signal WR_Sync: STD_LOGIC;
   signal WR_Reset: STD_LOGIC;
   signal WR: STD_LOGIC;
 
-  constant clock_period: time := 10 ns;
-  signal stop_the_clock: boolean;
+  -- constant clock_period: time := 10 ns;
+  -- signal stop_the_clock: boolean;
 
 begin
 
-  uut: walk_register port map ( WR_Sync  => WR_Sync,
+  uut: walk_register port map ( clk      => clk,
+                                WR_Sync  => WR_Sync,
                                 WR_Reset => WR_Reset,
                                 WR       => WR );
 
@@ -30,38 +33,38 @@ begin
   begin
   
     -- Put initialisation code here
-    WR_Sync <= '0';
-    WR_Reset <= '0';
-    wait for 10 ns;
-    
-    WR_Sync <= '0';
-    wait for 5 ns;
-    WR_Reset <= '1';
-    wait for 10 ns;
-    
     WR_Sync <= '1';
-    wait for 5 ns;
     WR_Reset <= '0';
-    wait for 10 ns;
+    wait for 30ns;
     
-    WR_Sync <= '1';
-    wait for 5 ns;
     WR_Reset <= '1';
-    wait for 10 ns;
+    wait for 30ns;
+    
+    WR_Reset <= '0';
+    wait for 30ns;
+
+    WR_Sync <= '0';
+    wait for 30ns;
+    
+    WR_Reset <= '1';
+    wait for 30ns;
+    
+    WR_Reset <= '0';
+    wait for 30ns;
 
     -- Put test bench stimulus code here
 
-    stop_the_clock <= true;
+    -- stop_the_clock <= true;
     wait;
   end process;
 
-  clocking: process
-  begin
-    -- while not stop_the_clock loop
-    --  WR_sync <= '0', '1' after clock_period / 2;
-    --  wait for clock_period;
-    -- end loop;
-    wait;
-  end process;
+  -- clocking: process
+  -- begin
+  --  while not stop_the_clock loop
+  --    clk <= '0', '1' after clock_period / 2;
+  --    wait for clock_period;
+  --  end loop;
+  --  wait;
+  -- end process;
 
 end;
--- HDL_lab1-2.srcs/sources_1/new/time_parameters.vhd
+++ HDL_lab1-2.srcs/sources_1/new/time_parameters.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 05/04/2020 04:37:23 PM
+-- Create Date: 05/09/2020 11:40:26 AM
 -- Design Name: 
--- Module Name: walk_register - Behavioral
+-- Module Name: time_parameters - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -21,7 +21,6 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,30 +31,17 @@ use IEEE.numeric_std.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity walk_register is
-    Port ( clk : in std_logic;
-           WR_Sync : in STD_LOGIC;
-           WR_Reset : in STD_LOGIC;
-           WR : out STD_LOGIC);
-end walk_register;
+entity time_parameters is
+    Port ( prog_sync : in STD_LOGIC;
+           time_parameter_selector : in STD_LOGIC_VECTOR (1 downto 0);
+           interval : in STD_LOGIC_VECTOR (1 downto 0);
+           time_value : in STD_LOGIC_VECTOR (3 downto 0);
+           value : out STD_LOGIC);
+end time_parameters;
 
-architecture Behavioral of walk_register is
-
-signal wr_value : std_logic := '0';
+architecture Behavioral of time_parameters is
 
 begin
 
-process (clk, WR_sync, WR_Reset)
-begin
-    if falling_edge (clk) then
-        if (WR_Reset = '1') then
-            wr_value <= '0';
-        elsif (WR_sync = '1') then
-            wr_value <= '1';
-        end if;
-    end if;
-    
-    WR <= wr_value;
-end process;    
-    
+
 end Behavioral;
