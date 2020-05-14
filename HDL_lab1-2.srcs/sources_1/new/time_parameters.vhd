ec1b94cd6c2527c71d0e461056707bb53cacd801
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2020-05-14 08:34:41 +0530

    updated time_parameters.vhdl

--- HDL_lab1-2.srcs/sources_1/new/time_parameters.vhd
+++ HDL_lab1-2.srcs/sources_1/new/time_parameters.vhd
@@ -32,16 +32,51 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity time_parameters is
-    Port ( prog_sync : in STD_LOGIC;
+    Port ( clk : in std_logic;
+           prog_sync : in STD_LOGIC;
            time_parameter_selector : in STD_LOGIC_VECTOR (1 downto 0);
            interval : in STD_LOGIC_VECTOR (1 downto 0);
            time_value : in STD_LOGIC_VECTOR (3 downto 0);
-           value : out STD_LOGIC);
+           value : out STD_LOGIC_VECTOR (3 downto 0));
 end time_parameters;
 
 architecture Behavioral of time_parameters is
+    signal t_base : std_logic_vector (3 downto 0) := "0110";
+    signal t_ext : std_logic_vector (3 downto 0) := "0011";
+    signal t_yel : std_logic_vector (3 downto 0) := "0010";
+    signal temp_value : std_logic_vector (3 downto 0) := "0000";
+    
+begin
+
+process(clk, prog_sync, time_parameter_selector, time_value)
 
 begin
 
+    if rising_edge(clk) then
+        if prog_sync = '1' then
+            case time_parameter_selector is
+                when "00" => t_base <= time_value;
+                when "01" => t_ext <= time_value;
+                when "10" => t_yel <= time_value;
+            end case;
+        end if;
+    end if;
+
+end process;
+
+process(clk, interval)
+
+begin
+
+    if rising_edge(clk) then
+        case interval is
+            when "00" => temp_value <= t_base;
+            when "01" => temp_value <= t_ext;
+            when "10" => temp_value <= t_yel;
+        end case;
+        value <= temp_value;
+    end if;
+
+end process;
 
 end Behavioral;
