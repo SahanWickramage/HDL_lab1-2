e16a3fa16ae6ecaa7be3679bfc7f3b68eb482bd0
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2020-05-16 12:35:33 +0530

    added timer

---
+++ HDL_lab1-2.srcs/sources_1/new/timer.vhd
@@ -0,0 +1,77 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/16/2020 11:43:43 AM
+-- Design Name: 
+-- Module Name: timer - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity timer is
+    Port ( one_hz_enable : in STD_LOGIC;
+           start_timer : in STD_LOGIC;
+           value : in STD_LOGIC_VECTOR (3 downto 0);
+           expired : out STD_LOGIC);
+end timer;
+
+architecture Behavioral of timer is
+    signal count: integer            := 0;
+    signal previous_state: std_logic := '0';
+    signal timer_running: std_logic  := '0';
+begin
+
+expired <= '0';
+
+process (start_timer)
+begin
+    if (start_timer = '1' and timer_running = '0') then
+        count <= to_integer(unsigned(value));
+        timer_running <= '1';
+    end if;
+end process;
+
+process (one_hz_enable, value)
+begin
+    if timer_running = '1' then
+        if count = 0 then
+            timer_running <='0';
+            previous_state <= '0';
+            expired <= '1';
+            expired <= '0' after 20 ns;
+        else
+            if (one_hz_enable = '1' and previous_state = '0') then
+                previous_state <= '1';
+                count <= count - 1;
+            elsif (one_hz_enable = '0' and previous_state = '1') then
+                previous_state <= '0';
+            end if;
+        end if;
+    end if;
+end process;
+
+end Behavioral;
